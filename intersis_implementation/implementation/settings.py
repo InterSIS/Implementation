215c69c2c49adeaac32a678737f15031a37a2091
Joseph Schilz <joseph@schilz.org>
2015-12-27 16:44:27 -0800

    Satisfy pylint.

--- intersis_implementation/implementation/settings.py
+++ intersis_implementation/implementation/settings.py
@@ -13,10 +13,11 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+import intersis_implementation.implementation.secret_settings as secret_settings
+
 CONFIGURATION = os.environ["DJANGO_CONFIGURATION"]
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-import intersis_implementation.implementation.secret_settings as secret_settings
 
 SECRET_KEY = secret_settings.SECRET_KEY
 
